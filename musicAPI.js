ccd0b0661e36726cbeae6b0811408f0eeb34b318
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-09-08 14:58:59 -0700

    Linked server with favSOngs

--- musicAPI.js
+++ musicAPI.js
@@ -5,21 +5,20 @@ let cache = {};
 
 async function getMusic (request, response, next) {
   try {
-    let key = 'Data';
 
     let timeToCache = 1000 * 60 * 60 * 24 * 30;
     // let cacheTest = 1000 * 20;
-    if (cache[key] && Date.now() - cache[key].timestamp < timeToCache) {
+    if (cache && Date.now() - cache.timestamp < timeToCache) {
       console.log('It\'s in the cache!');
-      res.status(200).send(cache[key].data);
+      response.status(200).send(cache.data);
 
     } else {
       console.log('It\'s not in the cache, so let\'s cache it!');
-    let url = `http://api.napster.com/v2.2/tracks/top?apikey=${process.env.MUSIC_API_KEY}&limit=50`;
+    let url = `http://api.napster.com/v2.2/tracks/top?apikey=${process.env.MUSIC_API_KEY}&limit=100`;
     let musicObj = await axios.get(url);
     console.log(musicObj.data);
     let selectedCity = musicObj.data.tracks.map(music => new Music(music));
-    cache[key] = {
+    cache = {
       data: selectedCity,
       timestamp: Date.now(),
     };
